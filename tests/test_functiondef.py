1174fe98d8d70da0516052bb1678086896da3412
Liran Nuna <liranuna@gmail.com>
2017-05-29 16:58:43 -0700

    Added unit tests for AST FunctionDef

---
+++ tests/test_functiondef.py
@@ -0,0 +1,100 @@
+from textwrap import dedent
+
+from ensure_trailing_commas.trailing_comma_finder import find_missing_trailing_commas
+
+
+def test_functiondef():
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a
+        ): pass
+    """)) == [16]
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            param=value
+        ): pass
+    """)) == [33]
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *,
+            param=value
+        ): pass
+    """)) == [40]
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *args,
+            param=value
+        ): pass
+    """)) == [44]
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *args,
+            param=value,
+            **kwargs
+        ): pass
+    """)) == [58]
+
+
+def test_no_add_exists():
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            param=value,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *,
+            param=value,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *args,
+            param=value,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+            a,
+            *args,
+            param=value,
+            **kwargs,
+        ): pass
+    """)) == []
+
+
+def test_no_add_ignore():
+    assert find_missing_trailing_commas(dedent("""
+        def test(): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        def test(
+
+        ): pass
+    """)) == []

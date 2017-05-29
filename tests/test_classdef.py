3f0a47ba1d1db55dc9ca3108b233d7cad6cda41c
Liran Nuna <liranuna@gmail.com>
2017-05-29 16:54:15 -0700

    Added unit tests for AST ClassDef

---
+++ tests/test_classdef.py
@@ -0,0 +1,78 @@
+from textwrap import dedent
+
+from ensure_trailing_commas.trailing_comma_finder import find_missing_trailing_commas
+
+
+def test_classdef():
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            b
+        ): pass
+    """)) == [25]
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            metaclass=b
+        ): pass
+    """)) == [35]
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            not_metaclass=b
+        ): pass
+    """)) == [39]
+
+
+def test_no_add_exists():
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            b,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            metaclass=b,
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+            a,
+            not_metaclass=b,
+        ): pass
+    """)) == []
+
+
+def test_no_add_ignore():
+    assert find_missing_trailing_commas(dedent("""
+        class Test(object): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(
+
+        ): pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test: pass
+    """)) == []
+
+    assert find_missing_trailing_commas(dedent("""
+        class Test(a, b, c): pass
+    """)) == []

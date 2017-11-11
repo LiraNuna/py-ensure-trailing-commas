838ef9d212e81fc4f99ecd3c2cd6869b5645d1b3
Liran Nuna <liranuna@gmail.com>
2017-11-10 21:12:22 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='ensure-trailing-commas',
     description='pre-commit hook that ensures trailing commas for python',
     url='https://github.com/LiraNuna/py-ensure-trailing-commas',
-    version='0.1.1',
+    version='0.2',
 
     author='Liran Nuna',
     author_email='liranuna@gmail.com',

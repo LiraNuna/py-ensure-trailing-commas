faf832c383a89b73d2448f5d8b01cac099493c29
Liran Nuna <liranuna@gmail.com>
2017-11-10 21:12:04 -0800

    Added command line arguments

--- ensure_trailing_commas/main.py
+++ ensure_trailing_commas/main.py
@@ -1,22 +1,21 @@
+import argparse
+
 import sys
 
 from ensure_trailing_commas.trailing_comma_finder import find_missing_trailing_commas
 
 
-def main():
-    script_name, *file_names = sys.argv
-
+def execute(args):
     errors = []
-    for filename in file_names:
-        with open(filename, 'r+', encoding='utf-8') as file:
-            file_contents = file.read()
+    for file in args.files:
+        file_contents = file.read()
+        missing_commas_finder = find_missing_trailing_commas(file_contents, filename=file.name)
 
-            missing_commas_finder = find_missing_trailing_commas(file_contents, filename=filename)
+        insertion_indexes = missing_commas_finder.get_insersion_indexes()
+        if not insertion_indexes:
+            continue
 
-            insertion_indexes = missing_commas_finder.get_insersion_indexes()
-            if not insertion_indexes:
-                continue
-
+        if args.autofix:
             for index in reversed(insertion_indexes):
                 file_contents = file_contents[:index] + ',' + file_contents[index:]
 
@@ -24,13 +23,31 @@ def main():
             file.write(file_contents)
             file.truncate()
 
-            for line_number, column in missing_commas_finder.get_insersion_coordinates():
-                errors.append(':'.join((filename, str(line_number), str(column))))
+        for line_number, column in missing_commas_finder.get_insersion_coordinates():
+            errors.append(':'.join((file.name, str(line_number), str(column))))
 
-    if errors:
+    if not args.silent and errors:
         print('Missing trailing commas found:')
         for error in sorted(errors):
             print('  ', error)
+
+    return len(errors)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Finds, fixes missing trailing commas in python source code.')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-f', '--auto-fix', dest='autofix', help='Automatically add missing trailing commas [default]', action='store_true', default=True)
+    group.add_argument('-F', '--no-auto-fix', dest='autofix', help='Turn off autofixing', action='store_false', default=False)
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-r', '--report', dest='silent', help='Report findings in a summary [default]', action='store_false', default=False)
+    group.add_argument('-s', '--silent', dest='silent', help='Turn off reporting', action='store_true', default=True)
+
+    parser.add_argument('files', metavar='FILE', type=argparse.FileType(mode='r+', encoding='utf-8'), nargs='+', help='A file to check')
+
+    sys.exit(execute(parser.parse_args()))
 
 
 if __name__ == '__main__':
